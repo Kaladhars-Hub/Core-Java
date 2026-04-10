44b47649895b1656963584e0041f7edcecaf08c0
Kaladhars-Hub <kaladharcareers@gmail.com>
2026-04-10 22:23:20 +0530

    Added basics: variables, methods, print statements

--- src/corejava/basics/DataTypes.java
+++ src/corejava/basics/DataTypes.java
@@ -5,7 +5,7 @@ package corejava.basics;
  */
 public class DataTypes {
 
-    // Default values demonstration (static variables)
+    // These are used to Check default values demonstration (static variables)
     static byte defaultByte;
     static int defaultInt;
     static boolean defaultBoolean;
@@ -14,6 +14,7 @@ public class DataTypes {
         System.out.println("=== Primitive Data Types Demo ===\n");
 
         // Declaration + Initialization
+        //Examples of different of data types
         byte b = 127;
         short s = 32000;
         int i = 2147483647;
@@ -22,6 +23,15 @@ public class DataTypes {
         double d = 3.1415926535;
         boolean flag = true;
         char ch = 'A';
+
+        // Checking Size (in bytes)
+        // System.out.println(Short.SIZE / 8);   // prints 2
+
+        // Checking Range
+        // System.out.println(Byte.MIN_VALUE);     // -128
+        // System.out.println(Byte.MAX_VALUE);     // 127
+        // System.out.println(Integer.MIN_VALUE);  // -2147483648
+        // System.out.println(Integer.MAX_VALUE);  // 2147483647
 
         System.out.println("Values:");
         System.out.println("byte   : " + b);
@@ -37,6 +47,9 @@ public class DataTypes {
         System.out.println("byte default    : " + defaultByte);
         System.out.println("int default     : " + defaultInt);
         System.out.println("boolean default : " + defaultBoolean);
+
+        // Checking Default Value
+       // System.out.println(i);   // prints 0 (default value of int)
 
         System.out.println("\nSummary: Primitive types store actual value directly and have fixed size.");
     }
