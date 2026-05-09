cc175fcdf41472def7e94bfbfdaca48fd9d03e8c
Kaladhars-Hub <kaladharcareers@gmail.com>
2026-05-10 00:14:27 +0530

    Added basics: Introduction,class,variables methods,print statements

---
+++ src/corejava/basics/Example.java
@@ -0,0 +1,35 @@
+package corejava.basics;
+
+public class Example {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 2; i++) {
+            System.out.println("Hello");
+        }
+    }
+}
+
+
+
+        //int i = 1;
+
+        //do {
+          //  System.out.println("Hello");
+            //i++;
+        //} while (i <= 5);
+    //}
+//}
+
+
+
+
+
+        // int i = 1;
+
+        //while (i <= 5) {
+          //  System.out.println("Hello");
+            //i++;
+   //     }
+   // }
+//}
--- src/corejava/basics/HelloWorld.java
+++ src/corejava/basics/HelloWorld.java
@@ -1,8 +1,12 @@
 package corejava.basics;
 
+// Class name should match file name
 class HelloWorld {
 
+    // Main method is the starting point of Java program
     public static void main() {
+
+        // Printing text on the console screen
         System.out.println("Hello World!");
 }
 }
